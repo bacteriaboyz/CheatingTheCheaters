03848e2b081d0277f7d2e7dde79f831444468482
Pablo Cárdenas Ramírez <pablocarderam@gmail.com>
2017-07-12 23:11:59 -0700

    Updated node.h

--- src/dStruct/node.h
+++ src/dStruct/node.h
@@ -2,21 +2,28 @@
 #define _NODE_H
 
 #include "types.h"
+#include "hash.h"
+#include "table.h"
 
 struct nodeBac
 {
-    struct nodeBac **neighbors;
-    cFloat *distances;
-    cInt len;
-    cInt deg;           // Equal to number of used nodes in neighbors
+    tableHash neighbors;    // Hash table with neighboring nodeBac structs
+    cInt numNei;            // Number of neighbors
 
-    cVec pos;
+    cVec pos;               // Vector with position coordinates (x,y,z)
 
-    cFloat fitness;
-    cInt resistant : 1; // 2-bit allocation (assigns 1 bit to this variable,
-                        // more efficient than booleans).
+    cFloat alp_e;           // Effective replication rate, 1/h
+    
+    cFloat gam_e;           // Effective death rate, 1/h
 
-    cInt used : 1;      // Used to avoid setting nulls
+    cFloat s_h;             // Plasmid uptake rate, 1/h
+
+    cFloat ome_e;           // Event occurrence rate, 1/h
+    
+    cInt enz : 1;           // 2-bit allocation (assigns 1 bit to this variable,
+                                // more efficient than booleans).
+
+    cInt used : 1;          // Used to avoid setting nulls
 };
 
 typedef struct nodeBac nodeBac;
