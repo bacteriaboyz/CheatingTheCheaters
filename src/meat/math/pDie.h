2e933ccc4b72c39a2f5f25c0c0a3ee1d3b9852fa
Pablo Cárdenas Ramírez <pablocarderam@gmail.com>
2017-07-13 10:02:32 -0700

    Updated antibiotic.c,h

---
+++ src/meat/math/pDie.h
@@ -0,0 +1,31 @@
+#ifndef _PDIE_H
+#define _PDIE_H
+
+#include "sim.h"
+#include "types.h"
+#include "graph.h"
+#include "limits.h"
+#include "node.h"
+#include "hash.h"
+#include "table.h"
+#include "bucket.h"
+#include "errors.h"
+#include "stack.h"
+#include "rng.h"
+#include "nn.h"
+#include "param.h"
+
+/* 
+ * Function updates concentration of antibiotic in blood according to renal
+ * renal clearance and new doses. 
+ * 
+ *  Arguments:  pointer to simulation struct, in order to access constants
+ * 
+ *  Returns:    void
+ * 
+ *  Errors:     ??
+ */
+
+void updateAB(simBac *sim);
+
+#endif
