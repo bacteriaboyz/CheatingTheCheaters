a8270a8894d45a502e59ac7253d2a42686603de9
Pablo Cárdenas Ramírez <pablocarderam@gmail.com>
2017-07-12 20:26:50 -0700

    New file structure

--- src/meat/loop/population.c
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-
-#include "population.h"
-
-cFloat populationEff(nodeBac *node, simBac *sim)
-{
-    return 1-1/(1+(1/(1-simBac->rep_0)-1)*exp(-1*simBac->max_Growth));
-    // 1 - logistic equation. x_0 is 1 - initial replication rate
-}
