fe2948ba6fa549f2eabb42fad65f950c832ddc6b
Pablo Cárdenas Ramírez <pablocarderam@gmail.com>
2017-07-18 15:47:18 -0700

    Fixed abBlood.c negative array index bug

--- src/meat/math/abBlood.c
+++ src/meat/math/abBlood.c
@@ -3,9 +3,14 @@
 
 void updateAB(simBac *sim)
 {
+    cFloat prev_t = 0; // time of last spike
+    if (sim->dose_num > 0) // if we've already started administering drugs,
+    {
+        prev_t = sim->param.doses_t[sim->dose_num-1]; // set time of last spike to that
+    }
     sim->c_b = sim->c_b_peak * \
         exp( -1 * sim->param.k_a * \
-        ( sim->t - sim->param.doses_t[sim->dose_num-1] ) \
+        ( sim->t - prev_t ) \
         / sim->param.v_w );
         // Exponential decay from last time spike
         // ( 1.0 - sim->param.t_s * sim->param.gam_ab ) * sim->c_b;
